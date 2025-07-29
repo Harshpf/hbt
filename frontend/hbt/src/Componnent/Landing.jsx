f310f6ab631d95efd375d7a77ddbc54c28d5de0f
aryanchoubey <aryan13aprilbuxar@gmail.com>
2025-07-29 22:24:53 +0530

    new update

--- frontend/hbt/src/Componnent/Landing.jsx
+++ frontend/hbt/src/Componnent/Landing.jsx
@@ -15,6 +15,7 @@ import { Navigation, Pagination, Mousewheel } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import './swiper.css'
 
 
 
